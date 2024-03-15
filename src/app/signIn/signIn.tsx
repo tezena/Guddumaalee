b6bb32900dc198b252614b444537204720b2d9c0
sami <samuelyeshambel9@gmail.com>
2024-03-15 11:11:01 +0300

    make the form center of the page modified:   src/app/signIn/signIn.tsx

--- src/app/signIn/signIn.tsx
+++ src/app/signIn/signIn.tsx
@@ -39,54 +39,48 @@ const SignInForm = () => {
   };
 
   return (
-    <Card className=" w-full sm:w-[70%] md:w-[50] lg:w-[40%] sm:p-8 p-4">
-    <Form {...form} className='space-y-4'>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
-      <div className="max-w-md w-full mx-auto">
-        
-        <div className="text-3xl font-bold text-gray-900 mt-2 text-center">Log Into Gudumaalee</div>
-      </div>
-        <div className='space-y-2 ' min-h-screen bg-gray-50 flex flex-col justify-center>
-          <FormField
-            control={form.control}
-            name='email'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-sm font-bold text-gray-600 block">Email</FormLabel>
-                <FormControl>
-                  <Input placeholder='mail@example.com' {...field}  className="w-full p-2 border border-gray-300 rounded mt-1" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='password'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input
-                    type='password'
-                    placeholder='Enter your password'
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <Button className='w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm"' type='submit'>
-          Sign in
-        </Button>
-      </form>
-      
-      
-     
-    </Form>
-   </Card>
+    <div className="flex justify-center items-center min-h-screen bg-gray-50">
+      <Card className="w-full sm:w-[70%] md:w-[50] lg:w-[40%] sm:p-8 p-4">
+        <Form {...form} className="space-y-4">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+            <div className="max-w-md w-full mx-auto">
+              <div className="text-3xl font-bold text-gray-900 mt-2 text-center">Log Into Gudumaalee</div>
+            </div>
+            <div className="space-y-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-sm font-bold text-gray-600 block">Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="mail@example.com" {...field} className="w-full p-2 border border-gray-300 rounded mt-1" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="Enter your password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm" type="submit">
+              Sign in
+            </Button>
+          </form>
+        </Form>
+      </Card>
+    </div>
   );
 };
 
