bc6ae507b0c942759cc22521b802823515c85596
sami <samuelyeshambel9@gmail.com>
2024-03-22 18:39:37 +0300

    .

--- src/app/client/lawyers/[id]/page.tsx
+++ src/app/client/lawyers/[id]/page.tsx
@@ -1,36 +1,30 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import LawyerDetail from './LawyerDetail'
-import { useParams } from 'next/navigation'
-import { Lawyer, data } from '@/app/data/lawyersMockData'
-import { LawyerProps } from '@/components/lawyersCard'
-const page = () => {
+import React, { useEffect, useState } from "react";
+import LawyerDetail from "./LawyerDetail";
+import { useParams } from "next/navigation";
+import { data } from "@/app/data/lawyersMockData";
+import { LawyerProps } from "@/components/lawyersCard";
 
-    const [lawyer,setLawyer]= useState<LawyerProps>({} as LawyerProps)
+const Page: React.FC = () => {
+  const [lawyer, setLawyer] = useState<LawyerProps>({} as LawyerProps);
+  const params = useParams();
 
-    const params =  useParams()
-function searchById(id:string){
-    let lyr = null
-
-    lyr  = data.find((indi)=>indi.id==id)
-
-    if(lyr){
-        setLawyer(lyr)
+  function searchById(id: string) {
+    const lyr = data.find((indi) => indi.id === id);
+    if (lyr) {
+      setLawyer(lyr);
     }
-}
+  }
 
-useEffect(()=>{
-    searchById(params.id as string)
-},[params])
+  useEffect(() => {
+    searchById(params.id as string);
+  }, [params]);
 
   return (
-    <div> 
-     
-
-
-      <LawyerDetail lawyer={lawyer}/>
+    <div>
+      <LawyerDetail lawyer={lawyer} lawyers={data} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
