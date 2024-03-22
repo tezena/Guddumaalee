dcdf84f326c8e6c5233a6e7d0a9dc590ce79e73d
sami <samuelyeshambel9@gmail.com>
2024-03-22 21:46:11 +0300

    add sidebar

--- src/app/client/lawyers/[id]/LawyerDetail.tsx
+++ src/app/client/lawyers/[id]/LawyerDetail.tsx
@@ -1,70 +1,87 @@
 import { LawyerProps } from "@/components/lawyersCard";
-import React from "react";
+import React, { useState } from "react";
 import ReactStars from "react-rating-stars-component";
 import Link from "next/link";
 
-const LawyerDetail: React.FC<{ lawyer: LawyerProps; lawyers: LawyerProps[] }> = ({ lawyer }) => {
+const LawyerDetail: React.FC<{ lawyer: LawyerProps; lawyers: LawyerProps[] }> = ({ lawyer, lawyers }) => {
+  const [hoveredLawyer, setHoveredLawyer] = useState<LawyerProps | null>(null);
+  const filteredLawyers = lawyers.filter((item) => item.id !== lawyer.id);
+
   return (
     <div className="container mx-auto mt-10 px-4 md:px-0">
-      <div className="bg-gray-100 p-4 rounded-lg md:p-8">
-        <div className="mb-4 md:flex md:items-center md:justify-between">
-          <Link href="/client/lawyers"  className="bg-[#7B3B99] text-white font-bold py-2 px-4 rounded transition duration-300 hover:bg-purple-700 inline-block mb-2 md:mb-0">
-          
-              Back
-            
-          </Link>
-          <div>
-            <Link href="/client/lawyers/{id}/chat" className="bg-[#7B3B99] text-white font-bold py-2 px-4 rounded hover:bg-purple-700 inline-block mr-2">
-             
-                Chat with Lawyer
-             
-            </Link>
-            <button className="bg-[#7B3B99] text-white font-bold py-2 px-4 rounded hover:bg-purple-700 inline-block">
-              Review & Rate
-            </button>
-          </div>
+      <div className="bg-gray-100 p-4 rounded-lg md:p-8 md:flex">
+        <div className="md:w-1/4 md:mr-8">
+          <h3 className="text-xl font-semibold text-gray-800 mb-4">Other Lawyers</h3>
+          <ul>
+            {filteredLawyers.map((otherLawyer) => (
+              <li
+                key={otherLawyer.id}
+                className="mb-2"
+                onMouseEnter={() => setHoveredLawyer(otherLawyer)}
+                onMouseLeave={() => setHoveredLawyer(null)}
+              >
+                <Link href={`/client/lawyers/${otherLawyer.id}`} className="text-blue-500 hover:underline">
+                {otherLawyer.name}
+                </Link>
+                {hoveredLawyer && hoveredLawyer.id === otherLawyer.id && (
+                  <div className="mt-1 bg-white shadow rounded-lg p-2">
+                    <p className="text-sm text-gray-600">{otherLawyer.des}</p>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
-
-        <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden grid grid-cols-1 gap-4">
-          <div className="relative">
-            <img
-              src={lawyer.imageUrl}
-              alt={lawyer.name}
-              className="w-full object-cover rounded-t-lg"
-            />
-            <div className="absolute bottom-0 left-0 p-6 bg-gray-800 bg-opacity-75 rounded-br-lg">
-              <h2 className="text-3xl font-semibold text-white">{lawyer.name}</h2>
+        <div className="md:w-3/4">
+          <div className="mb-4 flex justify-between items-center">
+            <Link
+              href="/client/lawyers"
+              className="bg-[#7B3B99] text-white font-bold py-2 px-4 rounded transition duration-300 hover:bg-purple-700 inline-block mb-2 md:mb-0"
+            >
+              Back
+            </Link>
+            <div>
+              <Link
+                href="/client/lawyers/{id}/chat"
+                className="bg-[#7B3B99] text-white font-bold py-2 px-4 rounded hover:bg-purple-700 inline-block mr-2"
+              >
+                Chat with Lawyer
+              </Link>
+              <button className="bg-[#7B3B99] text-white font-bold py-2 px-4 rounded hover:bg-purple-700 inline-block">
+                Review & Rate
+              </button>
             </div>
           </div>
-          <div className="p-4 md:p-8">
-            <h3 className="text-xl font-semibold text-gray-800 mb-4">
-              Personal Information
-            </h3>
-            <p className="text-gray-600 mt-2">{lawyer.des}</p>
-            <div className="flex items-center mt-4">
-              <ReactStars
-                count={5}
-                value={lawyer.rate}
-                edit={false}
-                size={24}
-                activeColor="#ffd700"
-              />
-              <span className="ml-2 text-gray-600">{lawyer.rate}/5</span>
+
+          <div className="max-w-4xl bg-white shadow-lg rounded-lg overflow-hidden grid grid-cols-1 gap-4">
+            <div className="relative">
+              <img src={lawyer.imageUrl} alt={lawyer.name} className="w-full object-cover rounded-t-lg" />
+              <div className="absolute bottom-0 left-0 p-6 bg-gray-800 bg-opacity-75 rounded-br-lg">
+                <h2 className="text-3xl font-semibold text-white">{lawyer.name}</h2>
+              </div>
             </div>
-            <hr className="my-6 border-t border-gray-200" />
-            <h3 className="text-xl font-semibold text-gray-800 mb-4">Experience</h3>
-            <ul className="list-disc list-inside">
-              <li>Position: Divorce Lawyer</li>
-              <li>Practice Area: Family Lawyer, Criminal Defence, Personal Injury</li>
-              <li>Experience: 10 Years</li>
-            </ul>
-            <hr className="my-6 border-t border-gray-200" />
-            <h3 className="text-xl font-semibold text-gray-800 mb-4">Education & Court Admissions</h3>
-            <ul className="list-disc list-inside">
-              <li>Admization Institute of Law and Technology, Juzment School of Management, Cambridge</li>
-              <li>Academy University School of Law, Boston, MA</li>
-              <li>The Syntify High School Of New York</li>
-            </ul>
+            <div className="p-4 md:p-8">
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">Personal Information</h3>
+              <p className="text-gray-600 mt-2">{lawyer.des}</p>
+              <div className="flex items-center mt-4">
+                <ReactStars count={5} value={lawyer.rate} edit={false} size={24} activeColor="#ffd700" />
+                <span className="ml-2 text-gray-600">{lawyer.rate}/5</span>
+              </div>
+              <hr className="my-6 border-t border-gray-200" />
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">Experience</h3>
+              <ul className="list-disc list-inside">
+                <li>Position: Divorce Lawyer</li>
+                <li>Practice Area: Family Lawyer, Criminal Defence, Personal Injury</li>
+                <li>Experience: 10 Years</li>
+              </ul>
+              <hr className="my-6 border-t border-gray-200" />
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">Education & Court Admissions</h3>
+              <ul className="list-disc list-inside">
+                <li>Admization Institute of Law and Technology, Juzment School of Management, Cambridge</li>
+                <li>Academy University School of Law, Boston, MA</li>
+                <li>The Syntify High School Of New York</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
