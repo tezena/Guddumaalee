92db95501ca5d4961e42ff9d784e2eb0e005475a
kid3234 <birhaneaddis2@gmail.co>
2024-03-28 14:52:30 +0300

    update

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,14 @@
 "use client";
-import React from "react";
-import Bar from "@/components/admincomponentd/bar";
+import React, { useEffect } from "react";
+import Chart1 from "@/components/admincomponentd/bar";
+import Chart2 from "@/components/admincomponentd/chart2";
+import Chart3 from "@/components/admincomponentd/chart3";
+
 import { useState } from "react";
 
 export function Admin() {
+
+  useEffect(()=>{},[])
   console.log("two.......");
   
   // const [count, setCount] = useState(0);
@@ -12,7 +17,7 @@ export function Admin() {
       <div className="w-full h-fit p-4">
         <h1 className="font-bold text-3xl ">Dashboard</h1>
       </div>
-      <div className="w-full lg:w-3/4 h-52 flex justify-between gap-3 shadow-md rounded-lg p-4 bg-white items-center">
+      <div className="w-full lg:w-3/4 h-72 flex justify-between gap-3 shadow-md rounded-lg p-4 bg-white items-center m-auto">
         <div className="flex gap-4 p-4 ">
           <svg
             className="w-8 h-8 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -27,21 +32,12 @@ export function Admin() {
           <p>530</p>
         </div>
 
-        <div className="w-1/3 p-4 flex justify-evenly">
-          <Bar
-            height={220}
-            s1={330}
-            s2={200}
-            clabel={"clients"}
-            label1={"Active"}
-            label2={"Inactive"}
-            // shouldRenderChart={true}
-            // setCount={setCount}
-          />
+        <div className="w-1/3 h-full p-4 flex justify-evenly">
+          <Chart1 />
         </div>
       </div>
-      <div className="w-full h-fit p-4 flex flex-col lg:flex lg:flex-row gap-8 lg:gap-20">
-        <div className="w-3/4 lg:w-1/4 h-20 flex flex-col gap-3 shadow-md rounded-lg p-4  bg-white items-center justify-center">
+      <div className="w-full h-fit p-4 flex flex-col lg:flex lg:flex-row gap-8 lg:gap-20 justify-evenly">
+        <div className="w-3/4 lg:w-1/4 h-72 flex flex-col gap-3 shadow-md rounded-lg p-  bg-white items-center justify-center">
           <div className="flex gap-4">
             <svg
               className="w-8 h-8 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -56,20 +52,12 @@ export function Admin() {
             <p>420</p>
           </div>
 
-          {/* <div className="w-1/3 p-4 flex justify-evenly">
-            <Bar
-              height={220}
-              s1={330}
-              s2={200}
-              clabel={"Lawyers"}
-              label1={"Active"}
-              label2={"Inactive"}
-              shouldRenderChart={true}
-              setCount={setCount}
-            />
-          </div> */}
+          <div className="w-full h-4/5 flex justify-evenly">
+            <Chart2 />
+          </div>
         </div>
-        <div className="w-3/4 lg:w-1/4 h-20 flex flex-col gap-3 shadow-md rounded-lg p-4  bg-white items-center justify-center ">
+
+        <div className="w-3/4 lg:w-1/4 h-72 flex flex-col gap-3 shadow-md rounded-lg p-4  bg-white items-center justify-center ">
           <div className="flex gap-4">
             <svg
               className="w-8 h-8 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -84,18 +72,9 @@ export function Admin() {
             <p>420</p>
           </div>
 
-          {/* <div className="w-1/3 p-4 flex justify-evenly">
-            <Bar
-              height={220}
-              s1={310}
-              s2={250}
-              clabel={"Cases"}
-              label1={"Active"}
-              label2={"Droped"}
-              shouldRenderChart={true}
-              setCount={setCount}
-            />
-          </div> */}
+          <div className="w-full h-64 flex justify-evenly">
+            <Chart3 />
+          </div>
         </div>
       </div>
     </div>
--- src/components/admincomponentd/bar.tsx
+++ src/components/admincomponentd/bar.tsx
@@ -1,147 +1,189 @@
 "use client";
-import { useState, useEffect } from "react";
-import ApexCharts from "apexcharts";
-interface BarProps {
-  height: number;
-  s1: number;
-  s2: number;
-  clabel: string;
-  label1: string;
-  label2: string;
-  // setCount: Function;
-  // shouldRenderChart: Boolean;
-}
-const getChartOptions = (
-  s1: number,
-  s2: number,
-  h: number,
-  clabel: string,
-  lable1: string,
-  lable2: string
-) => {
-  return {
-    series: [s1, s2],
-    colors: ["#1C64F2", "#E74694"],
-    chart: {
-      height: h,
-      width: "100%",
-      type: "donut",
-    },
-    stroke: {
-      colors: ["transparent"],
-      lineCap: "",
-    },
-    plotOptions: {
-      pie: {
-        donut: {
-          labels: {
-            show: true,
-            name: {
-              show: true,
-              fontFamily: "Inter, sans-serif",
-              offsetY: 20,
-            },
-            total: {
-              showAlways: true,
-              show: true,
-              label: clabel,
-              fontFamily: "Inter, sans-serif",
-              formatter: function (w: ApexCharts.Series) {
-                const sum = w.globals.seriesTotals.reduce(
-                  (a: number, b: number) => {
-                    return a + b;
-                  },
-                  0
-                );
-                return sum;
-              },
-            },
-            value: {
-              show: true,
-              fontFamily: "Inter, sans-serif",
-              offsetY: -20,
-              formatter: function (value: string) {
-                return value;
-              },
-            },
-          },
-          size: "80%",
-        },
-      },
-    },
-    grid: {
-      padding: {
-        top: -2,
-      },
-    },
-    labels: [lable1, lable2],
-    dataLabels: {
-      enabled: false,
-    },
-    legend: {
-      position: "left",
-      fontFamily: "Inter, sans-serif",
-    },
-    yaxis: {
-      labels: {
-        formatter: function (value: string) {
-          return value;
-        },
-      },
-    },
-    xaxis: {
-      labels: {
-        formatter: function (value: string) {
-          return value;
-        },
-      },
-      axisTicks: {
-        show: false,
-      },
-      axisBorder: {
-        show: false,
-      },
-    },
-  };
-};
-export function Bar({
-  height,
-  s1,
-  s2,
-  clabel,
-  label1,
-  label2,
-  // shouldRenderChart,
-  // setCount,
-}: BarProps) {
-  // const [shouldRender, setShouldRender] = useState(shouldRenderChart);
-console.log("one........");
+import React, { useEffect } from "react";
+import Chart from "chart.js/auto";
+
+export function chart() {
+  // useEffect(()=>{},[]);
 
   useEffect(() => {
-  console.log("this is the log 1");
-  const chartOptions = getChartOptions(s1, s2, height, clabel, label1, label2);
-  if (
-    document.getElementById("donut-chart") &&
-    typeof ApexCharts !== "undefined"
-  ) {
-    const chart = new ApexCharts(
-      document.getElementById("donut-chart"),
-      chartOptions
-    );
-    chart.render();
-  }
+    const ctx  = document.getElementById("donut-chart");
+    if (ctx !== null) {
+      new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: ["Red", "Blue", "Yellow"],
+          datasets: [
+            {
+              label: "My First Dataset",
+              data: [300, 50, 100],
+              backgroundColor: [
+                "rgb(255, 99, 132)",
+                "rgb(54, 162, 235)",
+                "rgb(255, 205, 86)",
+              ],
+              hoverOffset: 4,
+            },
+          ],
+        },
+      });
+    }
   }, []);
 
   return (
     <>
-      <div id="donut-chart" className="w-full "></div>
+      <div >
+        <canvas id="donut-chart"></canvas>
+      </div>
     </>
+
   );
 }
 
-export default Bar;
+export default chart;
 
-// useEffect(() => {
+// import { useState, useEffect } from "react";
+// import ApexCharts from "apexcharts";
+// interface BarProps {
+//   height: number;
+//   s1: number;
+//   s2: number;
+//   clabel: string;
+//   label1: string;
+//   label2: string;
+//   // setCount: Function;
+//   // shouldRenderChart: Boolean;
+// }
+// const getChartOptions = (
+//   s1: number,
+//   s2: number,
+//   h: number,
+//   clabel: string,
+//   lable1: string,
+//   lable2: string
+// ) => {
+//   return {
+//     series: [s1, s2],
+//     colors: ["#1C64F2", "#E74694"],
+//     chart: {
+//       height: h,
+//       width: "100%",
+//       type: "donut",
+//     },
+//     stroke: {
+//       colors: ["transparent"],
+//       lineCap: "",
+//     },
+//     plotOptions: {
+//       pie: {
+//         donut: {
+//           labels: {
+//             show: true,
+//             name: {
+//               show: true,
+//               fontFamily: "Inter, sans-serif",
+//               offsetY: 20,
+//             },
+//             total: {
+//               showAlways: true,
+//               show: true,
+//               label: clabel,
+//               fontFamily: "Inter, sans-serif",
+//               formatter: function (w: ApexCharts.Series) {
+//                 const sum = w.globals.seriesTotals.reduce(
+//                   (a: number, b: number) => {
+//                     return a + b;
+//                   },
+//                   0
+//                 );
+//                 return sum;
+//               },
+//             },
+//             value: {
+//               show: true,
+//               fontFamily: "Inter, sans-serif",
+//               offsetY: -20,
+//               formatter: function (value: string) {
+//                 return value;
+//               },
+//             },
+//           },
+//           size: "80%",
+//         },
+//       },
+//     },
+//     grid: {
+//       padding: {
+//         top: -2,
+//       },
+//     },
+//     labels: [lable1, lable2],
+//     dataLabels: {
+//       enabled: false,
+//     },
+//     legend: {
+//       position: "left",
+//       fontFamily: "Inter, sans-serif",
+//     },
+//     yaxis: {
+//       labels: {
+//         formatter: function (value: string) {
+//           return value;
+//         },
+//       },
+//     },
+//     xaxis: {
+//       labels: {
+//         formatter: function (value: string) {
+//           return value;
+//         },
+//       },
+//       axisTicks: {
+//         show: false,
+//       },
+//       axisBorder: {
+//         show: false,
+//       },
+//     },
+//   };
+// };
+// export function Bar({
+//   height,
+//   s1,
+//   s2,
+//   clabel,
+//   label1,
+//   label2,
+//   // shouldRenderChart,
+//   // setCount,
+// }: BarProps) {
+//   // const [shouldRender, setShouldRender] = useState(shouldRenderChart);
+// console.log("one........");
+
+//   useEffect(() => {
 //   console.log("this is the log 1");
+//   const chartOptions = getChartOptions(s1, s2, height, clabel, label1, label2);
+//   if (
+//     document.getElementById("donut-chart") &&
+//     typeof ApexCharts !== "undefined"
+//   ) {
+//     const chart = new ApexCharts(
+//       document.getElementById("donut-chart"),
+//       chartOptions
+//     );
+//     chart.render();
+//   }
+//   }, []);
 
-// }, []);
+//   return (
+//     <>
+//       <div id="donut-chart" className="w-full "></div>
+//     </>
+//   );
+// }
+
+// export default Bar;
+
+// // useEffect(() => {
+// //   console.log("this is the log 1");
+
+// // }, []);
