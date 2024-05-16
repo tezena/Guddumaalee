4c5b1864c8c97504d678139cfffc3b66342e3619
Yewe <82028523+ye-we@users.noreply.github.com>
2024-05-16 16:38:53 +0300

    lawyer file upload

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import CreateAccountForm from "./client/createAccount/createAccountForm";
-import LandingPage from "./landingPage/page";
-
-export default function Home() {
-  return <LandingPage />;
-}
