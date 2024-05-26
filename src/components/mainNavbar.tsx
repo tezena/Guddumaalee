b63dd437fcaea5032d6f0ea60204f2a9c358c197
Tewodros Nibret Adal <tewodrosnibretadal2012@gamil.com>
2024-05-26 22:08:32 +0300

    landing page with signIn button

--- src/components/mainNavbar.tsx
+++ src/components/mainNavbar.tsx
@@ -8,79 +8,85 @@ import { MdOutlineLocalPostOffice } from "react-icons/md";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import SearchInput from "./searchInput";
 import { Context } from "@/app/context/userContext";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { useSession } from "next-auth/react";
 import { HandleChat } from "@/app/chat/handleChat";
-
+import { ProfileDropdown } from "./ui/profileDropDown";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const {data:session}=useSession()
+  const { data: session } = useSession();
   const userType = session?.user?.image?.type;
 
   const handleNav = () => {
     setNav(!nav);
   };
 
-
   const context = useContext(Context);
-  const router=useRouter();
+  const router = useRouter();
   if (!context) {
     return null;
   }
-
-  
 
   return (
     <div className="absolute sticky top-0 bg-background/95 md:backdrop-blur md:text-black z-50 flex justify-between items-center h-20 min-w-screen mx-auto px-4 text-white border ">
       {/* Logo */}
       <div className="w-[15%] lg:pl-12">
-        <Link href={'/client'} > <h1 className="w-full text-3xl font-bold text-black "> <span className="text-[#7B3B99]">Gudu</span>maalee</h1> </Link>
+        <Link href={"/client"}>
+          {" "}
+          <h1 className="w-full text-3xl font-bold text-black ">
+            {" "}
+            <span className="text-[#7B3B99]">Gudu</span>maalee
+          </h1>{" "}
+        </Link>
       </div>
       <>
-      {
-        session && (
-          <> 
-      <div className="w-[30%]  md:flex hidden">
-        <SearchInput />
-      </div>
+        {session ? (
+          <>
+            <div className="w-[30%]  md:flex hidden">
+              <SearchInput />
+            </div>
 
-      <ul className="hidden md:flex items-center ">
-        <li className="py-4 px-2 rounded-xl m-1 cursor-pointer duration-300 hover:text-black hover:scale-110 ">
-          
-          <MdOutlineLocalPostOffice
-          onClick={HandleChat}
-            className="w-12 h-10 text-gray-400 hover:text-[#7B3B99]"
-          />
-         
-         
-        </li>
-        <li className="py-4 px-2 rounded-xl m-1 cursor-pointer duration-300 hover:text-black hover:scale-110  text-slate-500  ">
-          
-          {session.user.image?.type == "client"
-              ?   <Link href="/client/lawyers"><h3 className="text-2xl group-hover:font-bolder hover:text-[#7B3B99]">Lawyer</h3></Link>
-              : <Link href="/lawyers"><h3 className="text-2xl group-hover:font-bolder hover:text-[#7B3B99]">My Page</h3></Link>}
-       
-        </li>
-        <li className="py-4 px-2 rounded-xl m-1 duration-300 hover:scale-110">
-          {/* <IoPersonCircleSharp className="text-[#7B3B99] w-16 h-12" /> */}
-          <div className="flex p-1 bg-[#7B3B99] cursor-pointer items-center justify-center h-[40px] w-[40px] rounded-full ">
-                      <span className="text-xl text-white font-semibold capitalize ">
-                      {session?.user?.email?.slice(0, 1)}
-                        </span> 
-                    </div>
-        </li>
-      </ul>
+            <ul className="hidden md:flex items-center ">
+              <li className="py-4 px-2 rounded-xl m-1 cursor-pointer duration-300 hover:text-black hover:scale-110 ">
+                <MdOutlineLocalPostOffice
+                  onClick={HandleChat}
+                  className="w-12 h-10 text-gray-400 hover:text-[#7B3B99]"
+                />
+              </li>
+              <li className="py-4 px-2 rounded-xl m-1 cursor-pointer duration-300 hover:text-black hover:scale-110  text-slate-500  ">
+                {session.user.image?.type == "client" ? (
+                  <Link href="/client/lawyers">
+                    <h3 className="text-2xl group-hover:font-bolder hover:text-[#7B3B99]">
+                      Lawyer
+                    </h3>
+                  </Link>
+                ) : (
+                  <Link href="/lawyers">
+                    <h3 className="text-xl group-hover:font-bolder hover:text-[#7B3B99]">
+                      MyPage
+                    </h3>
+                  </Link>
+                )}
+              </li>
+              <li className="py-4 px-2 rounded-xl m-1 duration-300 hover:scale-110">
+                {/* <IoPersonCircleSharp className="text-[#7B3B99] w-16 h-12" /> */}
+                <ProfileDropdown />
+              </li>
+            </ul>
 
-      <div onClick={handleNav} className="block md:hidden text-black">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
+            <div onClick={handleNav} className="block md:hidden text-black">
+              {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+          </>
+        ) : (
+          <Button className=" rounded bg-[#7B3B99] px-8 py-3 text-sm font-medium text-white shadow hover:text-white focus:outline-none focus:ring  sm:w-auto">
+            <Link href="/signin">Sign In</Link>
+          </Button>
+        )}
       </>
-         )
-        }
-      </>
- 
+
       <ul
         className={
           nav
@@ -90,35 +96,21 @@ const Navbar = () => {
       >
         <h1 className="w-full text-3xl font-bold text-white m-4">Guddumalee</h1>
 
-   
-          <li
-         
-            className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
-          >
-             <span onClick={HandleChat}>
-              Chat
-             </span>
-          </li>
+        <li className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600">
+          <span onClick={HandleChat}>Chat</span>
+        </li>
 
-          <li
-         
-         className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
-       >
-           {userType == "client"
-              ?   <Link href="/client/lawyers">Lawyer</Link>
-              : <Link href="/lawyers">My Page</Link>}
-        
-       </li>
+        <li className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600">
+          {userType == "client" ? (
+            <Link href="/client/lawyers">Lawyer</Link>
+          ) : (
+            <Link href="/lawyers">My Page</Link>
+          )}
+        </li>
 
-       <li
-         
-         className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
-       >
-      
-           Profile
-        
-       </li>
-        
+        <li className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600">
+          Profile
+        </li>
       </ul>
     </div>
   );
---
+++ src/components/ui/profileDropDown.tsx
@@ -0,0 +1,75 @@
+import {
+    Cloud,
+    CreditCard,
+    Keyboard,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    MessageSquare,
+    Plus,
+    PlusCircle,
+    Settings,
+    User,
+    UserPlus,
+    Users,
+  } from "lucide-react"
+  
+  import { Button } from "@/components/ui/button"
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  import { useSession } from "next-auth/react"
+  import { signOut } from "next-auth/react";
+  
+  export function ProfileDropdown() {
+
+    const {data:session}=useSession()
+    
+    const handleLogOut=() => {
+        signOut({
+          redirect: true,
+          callbackUrl: "/",
+        });
+      }
+
+
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+        <div className="flex p-1 bg-[#7B3B99] cursor-pointer items-center justify-center h-[40px] w-[40px] rounded-full ">
+                      <span className="text-xl text-white font-semibold capitalize ">
+                      {session?.user?.email?.slice(0, 1)}
+                        </span> 
+                    </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem >
+            <LogOut className="mr-2 h-4 w-4" />
+            <span onClick={handleLogOut}>Log out</span>
+         
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+}
+  
