8c6b2fa91e20d818c51b4654b7bb457b30677692
Yewe <82028523+ye-we@users.noreply.github.com>
2024-06-14 10:35:47 +0300

    lawyer oop

--- src/server/user-management/Lawyer.ts
+++ src/server/user-management/Lawyer.ts
@@ -2,9 +2,22 @@ import { db } from "@/lib/db";
 import { Account } from "./Account";
 import bcrypt from "bcrypt";
 import { isAdmin, isAuthenticated } from "../checkRole";
+import { Court, Language, Specialty } from "@prisma/client";
 
 export class Lawyer extends Account {
-  static async add(email: string, password: string) {
+  static async add(
+    email: string,
+    password: string,
+    identification_card: string,
+    qualification: string,
+    languages: Language[],
+    specialties: Specialty[],
+    courts: Court[],
+    photo: string,
+    description: string,
+    cv: string | undefined,
+    resume: string | undefined
+  ) {
     const emailUsed =
       (await db.client.findFirst({ where: { email } })) ||
       (await db.lawyer.findFirst({ where: { email } }));
@@ -12,25 +25,69 @@ export class Lawyer extends Account {
       throw new Error("Email already used");
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await db.client.create({
+    const newUser = await db.lawyer.create({
       data: {
         email: email,
         password: hashedPassword,
+        identification_card: identification_card,
+        qualification: qualification,
+        languages: languages,
+        specialties: specialties,
+        courts: courts,
+        photo: photo,
+        description: description,
+        ...(cv && { cv: cv }),
+        ...(resume && { resume: resume }),
       },
     });
     return newUser;
   }
 
-  static async getAll() {
+  static async getUnverified() {
     await isAdmin();
-    const clients = await db.client.findMany({
+    const lawyers = await db.lawyer.findMany({
       select: {
         created_at: true,
+        cv: true,
         email: true,
         id: true,
+        identification_card: true,
+        isVerified: true,
+        qualification: true,
+        resume: true,
+        courts: true,
+        languages: true,
+        specialties: true,
         updatedAt: true,
       },
+      where: {
+        isVerified: false,
+      },
     });
-    return clients;
+    return lawyers;
+  }
+
+  static async getVerified() {
+    await isAdmin();
+    const lawyers = await db.lawyer.findMany({
+      select: {
+        created_at: true,
+        cv: true,
+        email: true,
+        id: true,
+        identification_card: true,
+        isVerified: true,
+        qualification: true,
+        resume: true,
+        courts: true,
+        languages: true,
+        specialties: true,
+        updatedAt: true,
+      },
+      where: {
+        isVerified: true,
+      },
+    });
+    return lawyers;
   }
 }
