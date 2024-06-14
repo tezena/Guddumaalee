cb46b495eadd2fdcd1b327478ab264728f457359
Yewe <82028523+ye-we@users.noreply.github.com>
2024-06-14 11:37:45 +0300

    profile dropdown updated

--- src/components/profileDropDown.tsx
+++ src/components/profileDropDown.tsx
@@ -1,40 +1,20 @@
 import {
-    Cloud,
-    CreditCard,
-    Keyboard,
-    LifeBuoy,
-    LogOut,
-    Mail,
-    MessageSquare,
-    Plus,
-    PlusCircle,
-    Settings,
-    User,
-    UserPlus,
-    Users,
-    HandHelping
-  } from "lucide-react"
-  
-  import { Button } from "@/components/ui/button"
-  import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuPortal,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuSub,
-    DropdownMenuSubContent,
-    DropdownMenuSubTrigger,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-  import { useSession } from "next-auth/react"
-  import { signOut } from "next-auth/react";
-  import Link from "next/link";
-  
-  export function ProfileDropdown() {
+  Cloud,
+  CreditCard,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+  Users,
+  HandHelping,
+} from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -68,34 +48,32 @@ export function ProfileDropdown() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div className="flex p-1 bg-[#7B3B99] cursor-pointer items-center justify-center h-[40px] w-[40px] rounded-full ">
-
-                      <span className="text-xl text-white font-semibold capitalize ">
-                      {session?.user?.email?.slice(0, 1)}
-                        </span> 
-                    </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
+          <span className="text-xl text-white font-semibold capitalize ">
+            {session?.user?.email?.slice(0, 1)}
+          </span>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <Link href={userType == "client" ? "" : "/lawyer/updateProfile"}>
+              {" "}
+              Profile
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuGroup>
+          {userType == "client" ? (
             <DropdownMenuItem>
-              <User className="mr-2 h-4 w-4" />
-              <Link href={userType == "client"? "":"/lawyer/updateProfile"}> Profile</Link>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuGroup>
-            {
-              userType=="client"?<DropdownMenuItem>
               <HandHelping className="mr-2 h-4 w-4" />
               <Link href="/client/dispute"> Dispute</Link>
-            </DropdownMenuItem>:""
-            } 
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem >
-            <LogOut className="mr-2 h-4 w-4" />
-            <span onClick={handleLogOut}>Log out</span>
-                   </DropdownMenuItem>
+            </DropdownMenuItem>
+          ) : (
+            ""
+          )}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
