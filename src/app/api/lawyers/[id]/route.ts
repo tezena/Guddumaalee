a9821ad6cd3ec64dcf23f1d86f58d1adefad6563
Yewe <82028523+ye-we@users.noreply.github.com>
2024-06-15 13:11:24 +0300

    added fields for lawyer detail

--- src/app/api/lawyers/[id]/route.ts
+++ src/app/api/lawyers/[id]/route.ts
@@ -17,57 +17,23 @@ export async function GET(
       created_at: true,
       cv: true,
       email: true,
+      phone_number: true,
+      photo: true,
+      full_name: true,
+      description: true,
       id: true,
       identification_card: true,
       isVerified: true,
       qualification: true,
       resume: true,
+      courts: true,
+      languages: true,
+      specialties: true,
       updatedAt: true,
+      balance: true,
     },
   });
   if (user != null) {
     return NextResponse.json({ user }, { status: 200 });
   } else return NextResponse.json({ message: "error on fetching user" });
 }
-
-// export async function PUT(
-//   req: NextRequest,
-//   { params }: { params: { id: string } }
-// ) {
-//   if (!params.id) {
-//     throw new Error("Provide the lawyer id");
-//   }
-//   const user = await req.json();
-
-//   const profile = await db.user.findUnique({
-//     where: {
-//       id: parseInt(params.id),
-//     },
-//   });
-
-//   if (profile != null) {
-//     try {
-//       const updatedProfile = await db.user.update({
-//         where: {
-//           id: profile.id,
-//         },
-//         data: {
-//           email: user.email,
-//         },
-//       });
-
-//       return NextResponse.json({
-//         message: "User updated successfully",
-//         profile: updatedProfile,
-//       });
-//     } catch (error) {
-//       console.error("Error updating user:", error);
-//       return NextResponse.json(
-//         { message: "Internal server error" },
-//         { status: 500 }
-//       );
-//     }
-//   } else {
-//     return NextResponse.json({ message: "User not found" }, { status: 404 });
-//   }
-// }
