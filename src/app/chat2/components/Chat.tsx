972bb7054ff22bacc8046d78be665afb7ef55831
kid3234 <birhaneaddis2@gmail.co>
2024-06-18 16:48:40 +0300

    offer update

--- src/app/chat2/components/Chat.tsx
+++ src/app/chat2/components/Chat.tsx
@@ -7,6 +7,9 @@ import { useSession } from "next-auth/react";
 //@ts-ignore
 import FileViewer from "react-file-viewer";
 import FileDownloader from "./fileDownloader";
+import { getClientCaseById } from "@/app/lawyer/api/offer";
+import { useQuery } from "@tanstack/react-query";
+
 
 interface iAppProps {
   data: {
@@ -28,12 +31,19 @@ export default function ChatComponent({ data }: iAppProps) {
   const [totalComments, setTotalComments] = useState(data);
   const messageEndRef = useRef<HTMLInputElement>(null);
   const { data: session } = useSession();
-
-  const userId = session?.user?.id;
+ //@ts-ignore
+  const userId = session?.user?.image?.id;
   const userEmail = session?.user?.email;
   //@ts-ignore
   const userType = session?.user?.image?.type;
   console.log(`user type2 :${userType}`);
+
+
+
+  const { data:clientcasedata, isLoading, error } = useQuery({
+    queryKey: ["clientlawyers"],
+    queryFn: () => getClientCaseById(userId),
+  });
 
   console.log(`this is toke: ${process.env.NEXT_PUBLIC_PUSHER_KEY}`);
 
@@ -76,33 +86,28 @@ export default function ChatComponent({ data }: iAppProps) {
               message.sender_email == userEmail ? (
                 <div className="flex items-center justify-end ">
                   {
-                    //@ts-ignore
-                    message.messageType == "text" ? (
+                    // @ts-ignore
+                    message.messageType === "text" ? (
                       <div className="rounded-lg bg-white p-4 shadow-md self-start mr-4">
                         {message.message}
                       </div>
-                    ) : (
-
-                      {
-                    //@ts-ignore
-
-                        message.messageType == "offer"?(<></>):
-                        (
-                        <div className="flex flex-col">
-                          <FileViewer
-                            fileType={message.fileType}
-                            filePath={message.message}
-                            onError={onError}
-                            style={{ width: "300px", height: "200px" }}
-                            className="rounded-md"
-                          />
-                          {
-                            //@ts-ignore
+                    ) : // @ts-ignore
+                    message.messageType === "offer" ? null : (
+                      <div className="flex flex-col">
+                        <FileViewer
+                          fileType={message.fileType}
+                          filePath={message.message}
+                          onError={onError}
+                          style={{ width: "300px", height: "200px" }}
+                          className="rounded-md"
+                        />
+                        {
+                          // Using fragment to wrap the comment
+                          <>
                             <FileDownloader fileUrl={message.message} />
-                          }
-                        </div>)                    
+                          </>
                         }
-                      
+                      </div>
                     )
                   }
 
