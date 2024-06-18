3cdc9489c2b6bfceb6ecf4769c06458c675ad909
Yewe <82028523+ye-we@users.noreply.github.com>
2024-06-18 04:57:10 +0300

    admin analytics

---
+++ src/app/api/dashboard/admin/clients/route.ts
@@ -0,0 +1,19 @@
+import { Admin } from "@/server/user-management/Admin";
+import { Lawyer } from "@/server/user-management/Lawyer";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: Response) {
+  try {
+    const analytics = await Admin.clientsCount();
+    return NextResponse.json({ analytics });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(`${error.message}`);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { error: "Couldn't get analytics." },
+      { status: 500 }
+    );
+  }
+}
